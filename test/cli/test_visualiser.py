d6a3277bc02b57eff853690afb82eecbbaba1c54
nicktgr15 <nicktgr15@gmail.com>
2015-06-08 20:48:09 +0100

    more unit tests

--- test/cli/test_visualiser.py
+++ test/cli/test_visualiser.py
@@ -1,12 +1,6 @@
-import sys
 import os
 from unittest import TestCase
 from sac.cli import visualiser
-import tempfile
-
-
-TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'testdata.html')
-
 
 
 class MyTestCase(TestCase):
@@ -14,12 +8,19 @@ class MyTestCase(TestCase):
     def setUp(self):
         self.stats_csv = os.path.join(os.path.dirname(__file__), '../fixtures/example_yaafe_stats.csv')
         self.no_stats_csv = os.path.join(os.path.dirname(__file__), '../fixtures/example_yaafe_no_stats.csv')
+        self.double_stats_csv = os.path.join(os.path.dirname(__file__), '../fixtures/example_yaafe_double_stats.csv')
 
-    def test_parse_yaafe_header(self):
+    def test_parse_yaafe_header_stats(self):
         header = visualiser.parse_yaafe_header(self.stats_csv)
         self.assertEqual(22050, header['samplerate'])
         self.assertEqual(15360, header['effective_step_size'])
 
+    def test_parse_yaafe_header_no_stats(self):
+        header = visualiser.parse_yaafe_header(self.no_stats_csv)
+        self.assertEqual(22050, header['samplerate'])
+        self.assertEqual(512, header['effective_step_size'])
 
-# if __name__ == '__main__':
-#     unittest.main()
+    def test_parse_yaafe_header_double_stats(self):
+        header = visualiser.parse_yaafe_header(self.double_stats_csv)
+        self.assertEqual(22050, header['samplerate'])
+        self.assertEqual(460800, header['effective_step_size'])
