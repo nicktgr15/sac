1416e04878644b8416daba680abe7faeec9d0165
nicktgr15 <nicktgr15@gmail.com>
2016-04-25 09:38:27 +0300

    incrementing version to 0.4.4 pypi issues

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(
         'sac.cli',
         'sac.model'
     ],
-    version='0.4.3',
+    version='0.4.4',
     description='Semantic Audio Companion',
     author='Nikolaos Tsipas',
     author_email='nicktgr15@yahoo.com',
     url='https://github.com/nicktgr15/sac',
-    download_url='https://github.com/nicktgr15/sac/releases/tag/0.4.3',
+    download_url='https://github.com/nicktgr15/sac/releases/tag/0.4.4',
     keywords=['semantic', 'audio', 'sox', 'similarity', 'matrix'],
     classifiers=[],
     install_requires=['matplotlib', 'numpy', 'scipy']
