3364c90086e7b7b0e1a0ca48146e7b852f8a614c
nicktgr15 <nicktgr15@gmail.com>
2016-07-29 20:12:28 +0300

    incrementing version for release

--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ setup(
         'sac.methods',
         'sac.methods.sm_analysis'
     ],
-    version='0.5.5',
+    version='0.6.0',
     description='Semantic Audio Companion',
     author='Nikolaos Tsipas',
     author_email='nicktgr15@yahoo.com',
     url='https://github.com/nicktgr15/sac',
-    download_url='https://github.com/nicktgr15/sac/releases/tag/0.5.5',
+    download_url='https://github.com/nicktgr15/sac/releases/tag/0.6.0',
     keywords=['semantic', 'audio', 'sox', 'similarity', 'matrix'],
     classifiers=[],
     install_requires=['matplotlib', 'numpy', 'scipy', 'scipy', 'sympy', 'peakutils', 'scikit-learn', 'pandas']
